3977a84528e3c159b62d79f7505fd22a35263e03
csronger <tarena@chenshurong.local>
2015-10-30 11:14:19 +0800

    版本一
    第一个github

---
+++ Day17_2_GitHub/Day17_2_GitHubTests/Day17_2_GitHubTests.m
@@ -0,0 +1,39 @@
+//
+//  Day17_2_GitHubTests.m
+//  Day17_2_GitHubTests
+//
+//  Created by tarena on 15/10/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Day17_2_GitHubTests : XCTestCase
+
+@end
+
+@implementation Day17_2_GitHubTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
